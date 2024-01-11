99baa16a38c3ce634ecd5c5b640ca8905030df6d
David Ferreira <mrjdavidfg@gmail.com>
2024-01-11 18:19:17 -0500

    Setup tanstack query

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,20 @@
+'use client'
+
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+
 import styles from './page.module.css'
 import { App } from '@/components/App/App'
 import { GeoLocationProvider } from '@/components/GeoLocationProvider'
+
+const queryClient = new QueryClient()
 
 export default function Home() {
   return (
     <main className={styles.main}>
       <GeoLocationProvider>
-        <App />
+        <QueryClientProvider client={queryClient}>
+          <App />
+        </QueryClientProvider>
       </GeoLocationProvider>
     </main>
   )
