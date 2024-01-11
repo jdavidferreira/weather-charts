87b0634aba76427059610fa42633466b6340e745
David Ferreira <mrjdavidfg@gmail.com>
2024-01-11 18:25:44 -0500

    Create api functions to fetch open weather api data

---
+++ src/api.ts
@@ -0,0 +1,44 @@
+import ky from 'ky'
+import { Position } from './types'
+
+export const api = ky.create({
+  prefixUrl: 'https://api.openweathermap.org/data/2.5/',
+})
+
+export function getCommonSearchParams(position: Position) {
+  return {
+    lat: position.latitude,
+    lon: position.longitude,
+    appid: process.env.NEXT_PUBLIC_OPEN_WEATHER_API_KEY!,
+  }
+}
+
+export async function fetchCurrentAirPollution(position: Position) {
+  const response = await api
+    .get('air_pollution', {
+      searchParams: getCommonSearchParams(position),
+    })
+    .json()
+
+  return response
+}
+
+export async function fetch5DayWeatherForecast(position: Position) {
+  const response = await api
+    .get('forecast', {
+      searchParams: getCommonSearchParams(position),
+    })
+    .json()
+
+  return response
+}
+
+export async function fetchCurrentWeather(position: Position) {
+  const response = await api
+    .get('weather', {
+      searchParams: getCommonSearchParams(position),
+    })
+    .json()
+
+  return response
+}
