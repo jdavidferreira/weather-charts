2f9ebd5e3d28df951b84653cf56f9839d85c97c6
David Ferreira <mrjdavidfg@gmail.com>
2024-01-13 15:50:50 -0500

    Replace styled-components with tailwindcss

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,6 @@
 
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 
-import styles from './page.module.css'
 import { App } from '@/components/App/App'
 import { GeoLocationProvider } from '@/components/GeoLocationProvider'
 
@@ -10,7 +9,7 @@ const queryClient = new QueryClient()
 
 export default function Home() {
   return (
-    <main className={styles.main}>
+    <main className="w-screen py-8 px-6">
       <GeoLocationProvider>
         <QueryClientProvider client={queryClient}>
           <App />
